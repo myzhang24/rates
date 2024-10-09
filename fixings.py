60a4fcb754a78f4c89c95c6559f99f35a5c82902
Mengyuan Zhang <myzhang24@gmail.com>
2024-10-09 15:45:52 -0400

    Sped up Fixings module

--- fixings.py
+++ fixings.py
@@ -2,22 +2,37 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
-__SOFR__FIXINGS = "fixings/SOFR.pkl"
-__SOFR_FILE = "fixings/SOFR.csv"
+__fixing_cache__ = None
 
-def get_SOFR_fixings():
-    df = pd.read_csv(__SOFR_FILE, index_col=0)
-    df.index = pd.to_datetime(df.index)
-    df = df.replace(".", np.nan).dropna().astype(float)
-    df.to_pickle(__SOFR__FIXINGS)
-    return df
+class FixingManager:
+    def __init__(self):
+        self.original_file = "fixings/SOFR.csv"
+        self.clean_file = "fixings/SOFR.pkl"
 
-def load_SOFR_fixings(from_date: dt.datetime, as_of: dt.datetime):
-    df = pd.read_pickle(__SOFR__FIXINGS)
-    return df.loc[from_date:as_of, "SOFR"]
+    def clean_fixings(self):
+        df = pd.read_csv(self.original_file, index_col=0)
+        df.index = pd.to_datetime(df.index)
+        df = df.replace(".", np.nan).dropna().astype(float)
+        df.to_pickle(self.clean_file)
+        return self
 
+    def load_fixings(self,):
+        global __fixing_cache__
+        __fixing_cache__ = pd.read_pickle(self.clean_file)
+        return self
+
+    def get_fixings(self, st: dt.datetime, et: dt.datetime):
+        if __fixing_cache__ is None:
+            self.load_fixings()
+        return __fixing_cache__[st: et]
+
+_SOFR_ = FixingManager()
 
 if __name__ == '__main__':
-    get_SOFR_fixings()
-    dg = load_SOFR_fixings(dt.datetime(2022, 1, 1), dt.datetime(2024, 10, 8))
+
+    # Clean fixings
+    _SOFR_.clean_fixings()
+
+    # Load fixings
+    dg = _SOFR_.get_fixings(dt.datetime(2022, 1, 1), dt.datetime(2024, 10, 8))
     exit(0)
