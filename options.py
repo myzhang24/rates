c32c590c2437e1ec083ce19a0725f16ea2559643
Mengyuan Zhang <myzhang24@gmail.com>
2024-10-16 15:49:42 -0400

    Finished option ticker parsing

--- options.py
+++ options.py
@@ -1,27 +1,35 @@
 import datetime as dt
+import re
 from dateutil.relativedelta import relativedelta
 from futures import _MONTH_CODES_
 from date_utils import get_nth_weekday_of_month, next_imm_date
 
 _QUARTERLY_CODE_ = {3: "H", 6: "M", 9: "U", 12: "Z"}
+_MIDCURVINESS_ = {
+    'SFR': 0,  # Standard
+    "SRA": 3,  # 3m mid curves
+    "SRR": 6,  # 6m mid curves
+    "SRW": 9,  # 9m mid curves
+    '0Q': 12,  # 1Y mid curves
+    '2Q': 24,  # 2Y mid curves
+    '3Q': 36,  # 3Y mid curves
+    '4Q': 48,  # 4Y mid curves
+    '5Q': 60,  # 5Y mid curves
+}
 
 def parse_sofr_option_ticker(ticker: str):
     ticker = ticker.upper()
-    # Define valid prefixes
-    prefixes = ['SFR', '0Q', '2Q', '3Q', '4Q', '5Q']
-
     # Try to match the prefix
     prefix = None
-    for p in prefixes:
+    for p in _MIDCURVINESS_.keys():
         if ticker.startswith(p):
             prefix = p
             break
-
     if not prefix:
         raise ValueError("Invalid ticker prefix for SOFR options")
 
     # Extract the rest of the ticker
-    rest = ticker[len(prefix):]
+    rest = re.findall(rf"{prefix}([FGHJKMNQUVXZ][\d+])", ticker)[0]
 
     if len(rest) not in [2, 3]:
         raise ValueError("Invalid ticker format")
@@ -49,16 +57,18 @@ def parse_sofr_option_ticker(ticker: str):
     expiry = imm_date - dt.timedelta(days=5)
     expiry = expiry.replace(hour=15) # 4pm CT 3pm EST
 
-    years_ahead = 0 if prefix == "SFR" else int(prefix[0]) if prefix[0] != '0' else 1
-    underlying_start = next_imm_date(expiry + relativedelta(years=years_ahead), 0)
+    # Add mid-curviness in months
+    months_ahead = _MIDCURVINESS_[prefix]
+    underlying_start = next_imm_date(expiry + relativedelta(months=months_ahead), 0)
     underlying_month_code = _QUARTERLY_CODE_[underlying_start.month]
     underlying_ticker = f"SFR{underlying_month_code}{underlying_start.year % 10}"
 
     return expiry, underlying_ticker
 
-# Example usage:
-exp, tick = parse_sofr_option_ticker('SFRZ3')
-print(f"Expiry: {exp}, Underlying: {tick}")
+if __name__ == '__main__':
+    # Example usage:
+    exp, tick = parse_sofr_option_ticker('SFRZ3')
+    print(f"Expiry: {exp}, Underlying: {tick}")
 
-exp, tick = parse_sofr_option_ticker('0QZ3')
-print(f"Expiry: {exp}, Underlying: {tick}")
+    exp, tick = parse_sofr_option_ticker('0QZ3')
+    print(f"Expiry: {exp}, Underlying: {tick}")
